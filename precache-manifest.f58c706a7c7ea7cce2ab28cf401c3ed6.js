02673bf11a5feb9dc6f9220580813c0ce633a1c2
Daniel Choi <daniel.won.choi@gmail.com>
2018-12-01 16:09:59 -0600

    Updates

---
+++ precache-manifest.f58c706a7c7ea7cce2ab28cf401c3ed6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ff91fdea2d8c2434e385c7f5cabd0ea",
+    "url": "/closers-society/static/media/logo.0ff91fde.jpg"
+  },
+  {
+    "revision": "ddc235557841f633892c30898d6d941f",
+    "url": "/closers-society/static/media/final design.ddc23555.png"
+  },
+  {
+    "revision": "5d10c3f88d45b67552c0",
+    "url": "/closers-society/static/js/runtime~main.5d10c3f8.js"
+  },
+  {
+    "revision": "a5120d47533df5696bad",
+    "url": "/closers-society/static/js/main.a5120d47.chunk.js"
+  },
+  {
+    "revision": "7d675b534befaf245934",
+    "url": "/closers-society/static/js/1.7d675b53.chunk.js"
+  },
+  {
+    "revision": "a5120d47533df5696bad",
+    "url": "/closers-society/static/css/main.ffb20de2.chunk.css"
+  },
+  {
+    "revision": "29fc12c9ca515aa01f36cfeb05f27bc5",
+    "url": "/closers-society/index.html"
+  }
+];
